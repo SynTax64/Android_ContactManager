6a571f7809d54bce3fd9ee3edb7ba13579314cd8
David A <davidaster007@gmail.com>
2021-09-14 10:23:38 +0200

    added test statements, if contact was successfully added to the database

--- app/src/main/java/com/example/contactmanager/MainActivity.java
+++ app/src/main/java/com/example/contactmanager/MainActivity.java
@@ -3,6 +3,12 @@ package com.example.contactmanager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.example.contactmanager.data.DatabaseHandler;
+import com.example.contactmanager.model.Contact;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DatabaseHandler database = new DatabaseHandler(MainActivity.this);
+
+        Contact person1 = new Contact();
+        person1.setName("David");
+        person1.setPhoneNumber("654565555631");
+        database.addContact(person1);
+        Log.d("addContact", "onCreate: " + "contact added");
+
+        List<Contact> contactList = database.getAllContacts();
+
+        for (Contact item : contactList) {
+            Log.d("addContact", "onCreate: " + item);
+        }
     }
 }
