ec3d91255c978f39eed9233a322a234c30555099
David A <davidaster007@gmail.com>
2021-09-14 10:13:58 +0200

    implementation onCreate,onUpgrade methods with database creation, added addContact, getContact and getAllContacts from database methods, added some string resource

--- app/src/main/java/com/example/contactmanager/data/DatabaseHandler.java
+++ app/src/main/java/com/example/contactmanager/data/DatabaseHandler.java
@@ -1,12 +1,19 @@
 package com.example.contactmanager.data;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
 
+import com.example.contactmanager.R;
+import com.example.contactmanager.model.Contact;
 import com.example.contactmanager.util.Util;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DatabaseHandler extends SQLiteOpenHelper {
 
@@ -15,12 +22,65 @@ public class DatabaseHandler extends SQLiteOpenHelper {
     }
 
     @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+    public void onCreate(SQLiteDatabase database) {
+        String CREATE_CONTACT_TABLE = "CREATE TABLE " + Util.TABLE_NAME +
+                "(" + Util.KEY_ID + " INTEGER PRIMARY KEY," + Util.KEY_NAME + " TEXT," + Util.KEY_PHONE_NUMBER + " TEXT)";
 
+        database.execSQL(CREATE_CONTACT_TABLE);
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-
+    public void onUpgrade(SQLiteDatabase database, int i, int i1) {
+        String DROP_TABLE = String.valueOf(R.string.db_drop);
+        database.execSQL(DROP_TABLE, new String[]{Util.DATABASE_NAME});
+        onCreate(database);
     }
+
+    public void addContact(Contact contact) {
+        SQLiteDatabase database = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+
+        values.put(Util.KEY_NAME, contact.getName());
+        values.put(Util.KEY_PHONE_NUMBER, contact.getPhoneNumber());
+
+        database.insert(Util.TABLE_NAME, null, values);
+        database.close();
+    }
+
+    public Contact getContact(int id) {
+        SQLiteDatabase database = this.getReadableDatabase();
+        Cursor cursor = database.query(Util.TABLE_NAME, new String[]{Util.KEY_ID, Util.KEY_NAME, Util.KEY_PHONE_NUMBER},
+                Util.KEY_ID + "=?", new String[]{String.valueOf(id)}, null, null, null);
+
+        if (cursor != null)
+            cursor.moveToFirst();
+
+        Contact contact = new Contact();
+        contact.setId(Integer.parseInt(cursor.getString(0)));
+        contact.setName(cursor.getString(1));
+        contact.setPhoneNumber(cursor.getString(2));
+
+        return contact;
+    }
+
+    public List<Contact> getAllContacts() {
+        List<Contact> contactList = new ArrayList<>();
+        SQLiteDatabase database = this.getReadableDatabase();
+        String selectAll = "SELECT * FROM " + Util.TABLE_NAME;
+        Cursor cursor = database.rawQuery(selectAll, null);
+
+        if (cursor.moveToFirst()) {
+            do {
+                Contact contact = new Contact();
+                contact.setId(Integer.parseInt(cursor.getString(0)));
+                contact.setName(cursor.getString(1));
+                contact.setPhoneNumber(cursor.getString(2));
+
+                contactList.add(contact);
+            } while (cursor.moveToNext());
+        }
+        return contactList;
+    }
+
+
 }
